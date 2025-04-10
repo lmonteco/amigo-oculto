522a87c028e8f7518dd621ac3cbde4082d67d7bf
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-10 10:51:28 -0300

    feat: embaralha array de amigos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-let inputAmigo = document.getElementById('nome-amigo');
-let listaAmigos = document.getElementById('lista-amigos');
-let amigosIncluidos = [];
+let amigo = document.getElementById('nome-amigo');
+let lista = document.getElementById('lista-amigos');
+let amigos = [];
 
 function adicionar() {
-    amigosIncluidos.push(`${inputAmigo.value}`);
+    amigos.push(`${amigo.value}`);
     
-    listaAmigos.textContent = amigosIncluidos;
+    lista.textContent = amigos;
+}
+
+function sortear() {
+    embaralhar(amigos);
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
