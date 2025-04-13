93790a465a962cbd112873d50652092dd1e02f5e
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-13 18:50:00 -0300

    chal: resolve o desafio 3

--- desafios/desafio1.js
+++ desafios/desafio1.js
@@ -8,3 +8,24 @@ console.log(novaLista);
 novaLista.pop(); //desafio 2
 
 console.log(novaLista);
+
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
+
+console.log(shuffle(novaLista));
+  
